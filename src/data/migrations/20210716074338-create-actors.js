f64c7cf63c808403f88fe29e2272a7f542c80384
God-damnit-all <53661808+God-damnit-all@users.noreply.github.com>
2025-05-24 13:34:59 -0500

    Update and rename 20210716074338-create-actors-all.js to 20210716074338-create-actors.js

--- src/data/migrations/20210716074338-create-actors.js
+++ src/data/migrations/20210716074338-create-actors.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Actors_all', {
+    await queryInterface.createTable('Actors', {
       id: {
         allowNull: false,
         autoIncrement: true,
